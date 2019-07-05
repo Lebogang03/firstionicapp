ff798a56df42220056e521ea1dd9945acc441954
lebogangjaq03 <lebogangjaq03@gmail.com>
2019-07-05 13:03:20 +0200

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+name;
+surname;
+age;
+
+  constructor(private route:Router) {}
+nextPage()
+{
+  this.route.navigateByUrl('details')
+
+  console.log(this.name);
+  console.log(this.surname);
+  console.log(this.age);
+}
+
+
+
+
+}
